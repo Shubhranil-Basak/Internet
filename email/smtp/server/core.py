3f057c026a3647721ee66e9cddb98dd599673b9b
Shubhranil Basak <Shubhranil.Basak@iiitb.ac.in>
2025-06-21 03:22:24 +0530

    more modularized

--- email/smtp/server/core.py
+++ email/smtp/server/core.py
@@ -11,22 +11,30 @@ class SMTPServer:
         self.socket.bind((self.host, self.port))
 
 
-    def parse_command(self, data):
+    def handle_helo(self, data):
+        return b"250 Hello\r\n"
 
-            if data.upper().startswith("HELO"):
-                return b"250 Hello\r\n"
-            
-            elif data.upper().startswith("MAIL FROM:"):
-                return b"250 OK\r\n"
-            
-            elif data.upper().startswith("RCPT TO:"):
-                return b"250 OK\r\n"
-            
-            elif data.upper() == "DATA":
-                return b"250 Message received\r\n"
-            
-            elif data.upper() == "QUIT":
-                return b"221 Bye\r\n"
+    def handle_mail_from(self, data):
+        sender = data.split(":", 1)[1].strip()
+        return sender, b"250 OK\r\n"
+
+    def handle_rcpt_to(self, data):
+        recipient = data.split(":", 1)[1].strip().strip("<>")
+        return recipient, b"250 OK\r\n"
+
+    def handle_data_start(self):
+        return b"354 End with . on a line\r\n"
+
+    def process_data_body(self, data_body_lines, sender, recipient):
+        content = f"From: {sender}\nTo: {recipient}\n" + "\n".join(data_body_lines)
+        self.save_email(recipient, content)
+        return b"250 Message received\r\n"
+
+    def handle_quit(self):
+        return b"221 Bye\r\n"
+
+    def handle_unknown(self):
+        return b"500 Command not recognized\r\n"
 
     def save_email(self, recipient, content):
         mailbox = os.path.join(MAILBOX_DIR, recipient)
